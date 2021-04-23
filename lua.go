88c7403ecce41d276bd26b90102d8a41f27c2c12
DGHeroin <DGHeroin@gmail.com>
2021-04-23 19:52:16 +0800

    no message

--- lua.go
+++ lua.go
@@ -20,6 +20,22 @@ import (
     "sync/atomic"
     "unsafe"
 )
+const (
+    LUA_MULTRET       = C.LUA_MULTRET
+)
+
+const (
+    LUA_TNONE          = int(C.LUA_TNONE)
+    LUA_TNIL           = int(C.LUA_TNIL)
+    LUA_TNUMBER        = int(C.LUA_TNUMBER)
+    LUA_TBOOLEAN       = int(C.LUA_TBOOLEAN)
+    LUA_TSTRING        = int(C.LUA_TSTRING)
+    LUA_TTABLE         = int(C.LUA_TTABLE)
+    LUA_TFUNCTION      = int(C.LUA_TFUNCTION)
+    LUA_TUSERDATA      = int(C.LUA_TUSERDATA)
+    LUA_TTHREAD        = int(C.LUA_TTHREAD)
+    LUA_TLIGHTUSERDATA = int(C.LUA_TLIGHTUSERDATA)
+)
 
 type (
     GoFunction        func(L *State) int
